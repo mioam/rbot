34d80c41adc18a8e48c8175df6076af3a29774ff
miaom <1678068845@qq.com>
2026-07-09 14:02:55 +0800

    sync

--- sync.sh
+++ sync.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+set -euo pipefail
+
+# Usage:
+#   ./sync.sh model1 exp1 29999
+#   ./sync.sh model1 '*' 29999
+#   ./sync.sh '*' '*' 29999
+
+MODEL=${1:-*}
+EXP=${2:-*}
+STEP=${3:?Missing checkpoint step}
+
+REMOTE="a800:/data/miaozhuochen/rbot/checkpoints"
+
+echo "Downloading:"
+echo "  model = $MODEL"
+echo "  exp   = $EXP"
+echo "  step  = $STEP"
+
 rsync -avm \
-  --include='*/' \
-  --include='*/*/*/params/***' \
-  --include='*/*/*/assets/***' \
-  --include='*/*/config.yaml' \
-  --exclude='*' \
-  ${SERVER}/checkpoints/ ./checkpoints
+    --include='*/' \
+    --include="${MODEL}/${EXP}/config.yaml" \
+    --include="${MODEL}/${EXP}/${STEP}/params/***" \
+    --include="${MODEL}/${EXP}/${STEP}/assets/***" \
+    --exclude='*' \
+    "$REMOTE" .
